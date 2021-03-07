c72b040df8843978426b029bc5c65b1b8658f689
Burak YAVAS <burakyvs0@gmail.com>
2021-03-07 15:28:17 +0300

    Private Chat added.

--- PracticeSignalR.Web/Hubs/MessageHub.cs
+++ PracticeSignalR.Web/Hubs/MessageHub.cs
@@ -9,19 +9,58 @@ namespace PracticeSignalR.Web.Hubs
     public class MessageHub : Hub
     {
         public static List<Tuple<string, string>> AllMessages = new List<Tuple<string, string>>();
-        private static IDictionary<string, string> clients = new Dictionary<string, string>();
-        public Task SendMessageToAll(string userName, string message)
+        public static List<Tuple<string, string>> ClientUserNames = new List<Tuple<string, string>>();
+        public Task SendMessageToAll(string senderUserName, string message)
         {
-            AllMessages.Add(Tuple.Create(userName, message));
-            return Clients.All.SendAsync("ReceiveMessage", userName, message);
+            AllMessages.Add(Tuple.Create(senderUserName, message));
+            return Clients.All.SendAsync("ReceiveMessage", senderUserName, "", message, false);
         }
 
+        public Task SendMessageToClient(string receiverConnectionId, string senderUserName, string receiverUserName, string message){
+            if(receiverConnectionId != Context.ConnectionId)
+                return Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", senderUserName, receiverUserName, message, true);
+            return Task.FromResult(0);
+        }
+        public Task SendMessageToCaller(string senderUserName, string receiverUserName, string message){
+            if(receiverUserName == senderUserName)
+                return Clients.Caller.SendAsync("ReceiveMessage", senderUserName, "", message, true);
+            else
+                return Clients.Caller.SendAsync("ReceiveMessage", senderUserName, receiverUserName, message, true);
+        }
+
+        /*public override async Task OnConnectedAsync(){
+            await base.OnConnectedAsync();
+        }*/
+
         public override async Task OnDisconnectedAsync(Exception ex){
+            foreach(var client in ClientUserNames){
+                if(client.Item1 == Context.ConnectionId){
+                    ClientUserNames.Remove(client);
+                    break;  
+                }        
+            }
+            await Clients.All.SendAsync("GetDisconnectedUsers", Context.ConnectionId);
             await base.OnDisconnectedAsync(ex);
         }
 
+        public Task UserConnected(string userName){
+            ClientUserNames.Add(Tuple.Create(Context.ConnectionId, userName));
+            return Clients.All.SendAsync("GetConnectedUser", Context.ConnectionId, userName);
+        }
+
+        public Task GetCallerConnectionId(){
+            return Clients.Caller.SendAsync("GetCallerConnectionId", Context.ConnectionId);
+        }
+
+        
+
+        public IEnumerable<Task> GetAllUsers(){
+            foreach(var client in ClientUserNames){
+                yield return Clients.Caller.SendAsync("GetAllConnectedUsers", client.Item1, client.Item2);
+            }
+        }
         public IEnumerable<Task> GetAllMessages(){
-            foreach(var message in AllMessages){
+           foreach(var message in AllMessages){
                 if(AllMessages.Last() == message){
                     yield return Clients.Caller.SendAsync("ReceiveAllMessages", message.Item1, message.Item2, true);
                     break;
--- PracticeSignalR.Web/wwwroot/js/message.js
+++ PracticeSignalR.Web/wwwroot/js/message.js
@@ -6,7 +6,6 @@ var connection = new signalR.HubConnectionBuilder()
     .build();
 
 connection.on("ReceiveAllMessages", function(userName, message, finished){
-        console.log("aha for, ", message);
         const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
         var div = document.createElement("div");
         div.innerHTML = userName + ": " + msg;
@@ -16,17 +15,60 @@ connection.on("ReceiveAllMessages", function(userName, message, finished){
             document.getElementById("messages").appendChild(div);
         }
 });
-connection
 
-connection.on("ReceiveMessage", function(userName, message){
+connection.on("ReceiveMessage", function(senderUserName, receiverUserName, message, isPrivate){
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var div = document.createElement("div");
-    div.innerHTML = userName + ": " + msg ;
+    if(!isPrivate){
+        div.innerHTML = senderUserName + ": " + msg ;
+    }else{
+        if(receiverUserName == ""){
+            div.innerHTML = senderUserName + " (<span style='color:blue;'>selfmsg</span>): " + msg ;
+        }else{
+            div.innerHTML = senderUserName + " to (<span style='color:red;'>" + receiverUserName + "</span>): " + msg ;
+        }
+        
+    }
+    
     document.getElementById("messages").appendChild(div);
 });
 
+connection.on("GetConnectedUser", function(connectionId, userName){
+        var option = document.createElement("option");
+        option.innerHTML = userName;
+        option.value = connectionId;
+        var sendToSelect = document.getElementById("sendToSelect");
+        var searchOptionWithId = document.getElementById(connectionId);
+        if(searchOptionWithId == null)
+            sendToSelect.appendChild(option);
+});
+
+connection.on("GetAllConnectedUsers", function(connectionId, userName){
+    var option = document.createElement("option");
+    option.innerHTML = userName;
+    option.value = connectionId;
+    document.getElementById("sendToSelect").appendChild(option);
+});
+
+connection.on("GetDisconnectedUsers", function(connectionId){
+    var sendToSelect = document.getElementById("sendToSelect");
+    for(var i = 0; i < sendToSelect.length; i++){
+        if(sendToSelect.options[i].value == connectionId)
+            sendToSelect.remove(i);
+    }
+})
+
 connection.start()
     .then(() => connection.invoke("GetAllMessages").catch(function(err) {
+        return console.error(err.toString());
+    }))
+    .then(() => connection.invoke("GetAllUsers").catch(function(err){
+        return console.error(err.toString());
+    }))
+    .then(() => connection.invoke("UserConnected", document.getElementById("userName").value).catch(function(err){
+        return console.error(err.toString());
+    }))
+    .then(() => connection.invoke("GetCallerConnectionId").then(function(connectionId){ console.log(connectionId);}).catch(function(err) {
         return console.error(err.toString());
     }))
     .catch(function(err) {
@@ -35,12 +77,25 @@ connection.start()
 
 document.getElementById("sendButton").addEventListener("click", function(event){
     var message = document.getElementById("message").value;
-    var userName = document.getElementById("userName").value;
+    var senderUserName = document.getElementById("userName").value;
     if(message != ""){
         document.getElementById("message").value = "";
-        connection.invoke("SendMessageToAll", userName, message).catch(function(err) {
-            return console.error(err.toString());
-        });
+        var sendToSelect = document.getElementById("sendToSelect");
+        var sendToWhoId = sendToSelect.options[sendToSelect.selectedIndex].value;
+        var sendToWhoUserName = sendToSelect.options[sendToSelect.selectedIndex].innerHTML;
+        if(sendToWhoId == ""){
+            connection.invoke("SendMessageToAll", senderUserName, message).catch(function(err) {
+                return console.error(err.toString());
+            });
+        }else{
+            connection.invoke("SendMessageToClient", sendToWhoId, senderUserName, sendToWhoUserName, message).catch(function(err){
+                return console.error(err.toString());
+            });
+            connection.invoke("SendMessageToCaller", senderUserName, sendToWhoUserName, message).catch(function(err){
+                return console.error(err.toString());
+            });
+        }
+        
     }
     event.preventDefault();
 });
